d6bc616852bd37f08d7d60fca75a3c88bfff29fc
Shubhrendu Tripathi <11039534+shubhrendutripathi@users.noreply.github.com>
2020-05-02 03:52:30 +0530

    added guid

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # C-Sharp
+This repository contains a collection of sample code, recipes, snippets, etc for the C# programming language.
+
+## Author
+Shubhrendu Tripathi
+
---
+++ guid/Main.cs
@@ -0,0 +1,16 @@
+/////////////////////////////////////////
+//
+// Shubhrendu Tripathi (c) 2019
+//
+// GPL V3
+//
+/////////////////////////////////////////
+
+using System;
+
+class Program {
+	static void Main(string[] args) {
+		Console.WriteLine(System.Guid.NewGuid().ToString());
+	}
+}
+
